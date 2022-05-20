439599f5575b6166851f16be608e7a2e37addda6
Vladimir Menkov <vmenkov@gmail.com>
2022-05-20 01:48:28 -0400

    switching to using a HashMap

--- src/edu/rutgers/pharma3/Splitter.java
+++ src/edu/rutgers/pharma3/Splitter.java
@@ -11,14 +11,19 @@ import java.util.*;
 **/
 
 public class Splitter extends If {
-  
-    /** Local copy of super.receivers (Receivers registered with the provider) */
-    ArrayList<Receiver> myReceivers = new ArrayList<Receiver>();
-  
-    /** fractions[j] specifies the fraction of the input that will
-	be sent to the j-th receiver */
-    ArrayList<Double> fractions = new ArrayList<Double>();
-    	
+
+
+    private static class RData {
+	/** fractions[j] specifies the fraction of the input that will
+	    be sent to the j-th receiver */
+	final double fraction;
+	/** How much resource has been given to this receiver so far */
+	double given=0;
+	RData(double f) { fraction = f; }
+    }
+
+    private HashMap<Receiver, RData> data = new HashMap<>();
+	
     void throwDoNotUse()        {
         throw new RuntimeException("Splitters do not respond to addReceiver(Receiver).  Instead, use addReceiver(Receiver, fraction).");
     }
@@ -33,53 +38,36 @@ public class Splitter extends If {
     }
     
     public boolean addReceiver(Receiver receiver, double fraction)      {
-	if (myReceivers.contains(receiver)) throw new IllegalArgumentException("Duplicate addReceiver())");
+	if (data.containsKey(receiver))  throw new IllegalArgumentException("Duplicate addReceiver())");
 	if (fraction < 0) throw new IllegalArgumentException("Fractions must be non-negatitve; given " + fraction);
-	fractions.add(fraction);
-	myReceivers.add(receiver);
-	boolean result = super.addReceiver(receiver);
-        return result;
+	data.put(receiver, new RData(fraction));
+	return super.addReceiver(receiver);
     }
-
-    Vector<Double> givenToEach = new Vector<Double>();
 
     public boolean removeReceiver(Receiver receiver)        {
-	int j = myReceivers.indexOf(receiver);
-	if (j<0)  return false;
-	fractions.remove(j);
-	myReceivers.remove(j);
-	if (givenToEach.size()>j) givenToEach.remove(j);
-        return super.removeReceiver(receiver);
+	data.remove(receiver);
+	return super.removeReceiver(receiver);
     }
 
 
-    double totalAcecepted=0;
+    double totalAccepted=0;
 
-    private double lastAmt = 0;
-    private int chosenJ = -1;
-    
-    /** This is a wrapper around Filter.offerReceiver(Receiver receiver, double atMost),
-	as "If" has no method of this name. Is used so that we can adjust "givenToEach" amounts
-	after we know that offerReceiever() has been successful
-	(i.e. the receiver has accepted the resource)
-     */
-    protected boolean offerReceiver(Receiver receiver, double atMost) {
-	// This method gets called by Provider.offerReceiver(...)
-	// with an argument that has been computed by Splitter.selectReceiver(),
-	// and that  Splitter.selectReceiver() call is supposed to have
-	// set lastAmt and chosenJ. Or at least that's the idea!
-	boolean z = super.offerReceiver(receiver, atMost);
-	//System.out.println("Splitter: offerReceiver done; chosenJ="+ chosenJ +", for amt=" + lastAmt);
-	if (z) {
-	    // FIXME: should use receiver.getLastAcceptedOffers().get(0) instead
-             
 
-	    
-	    totalAcecepted+= lastAmt;
-	    double x = givenToEach.get(chosenJ) + lastAmt;
-	    givenToEach.set(chosenJ, x);
-	}
-	return z;
+    /** This is called after a successful accept() by a downstream receiver */
+    public void offerSuccessful(Receiver receiver, Resource originalResource, Resource revisedResource) {
+	RData d = data.get(receiver);
+	if (d==null) throw new  IllegalArgumentException("Unknown receiver: " + receiver);
+	double givenAmt=0;
+	if (originalResource  instanceof CountableResource) {
+	    givenAmt = originalResource.getAmount() -  revisedResource.getAmount();
+	} else if (originalResource instanceof Batch) {
+	    givenAmt = ((Batch)originalResource).getContentAmount();
+	} else { // Entity. We know that one was accepted
+	    givenAmt = originalResource.getAmount();
+	    if (givenAmt!=1) throw new  IllegalArgumentException("We thought Entity.getAmount() always returns 1...");
+	}	
+	totalAccepted += givenAmt;
+	d.given  += givenAmt;
     }
 
 
@@ -92,48 +80,40 @@ public class Splitter extends If {
 	@param receivers  This should be exactly the array from Provider.receivers, or this method will break.
      */    
     public Receiver selectReceiver(ArrayList<Receiver> receivers, Resource amount) {
- 
-	if (receivers.size()!=myReceivers.size()) throw new IllegalArgumentException("Receivers array size mismatch");
-	int k=0;
-	for(Receiver r: receivers) {
-	    if (r!=myReceivers.get(k))  throw new IllegalArgumentException("Receivers array  mismatch in position " +k);
-	    k++;
-	}
-
-	
-	if (givenToEach.size() < fractions.size()) {
-	    int j = givenToEach.size();
-	    givenToEach.setSize( fractions.size());
-	    while( j<givenToEach.size()) givenToEach.set(j++, new Double(0));	    
-	}
-
-	int sumF=0;
-	for(Double x: fractions)	    sumF += x;
-
 	if (receivers.size()==0) throw new IllegalArgumentException("No receivers!");
-	if (receivers.size()!= fractions.size()) throw new AssertionError("receivers.size()!=fractions.size()");
+	
+	int sumF=0;
+	for(Receiver r: receivers) {
+	    RData d = data.get(r);
+	    if (d==null) throw new IllegalArgumentException("Unknown receiver: " + r);	   
+	    sumF += d.fraction;
+	}
 	if (sumF == 0) throw new IllegalArgumentException("All fractions are zero!");
+	
+	    //	if (receivers.size()!= fractions.size()) throw new AssertionError("receivers.size()!=fractions.size()");
+
 
 	// How much "stuff" are we offering?
 	// Here we set lastAmt for later use in offerReceiver (if accept() succeeds)
-	double amt = lastAmt =
+	double amt = 
 	    (amount instanceof Batch)? ((Batch)amount).getContentAmount() :
 	    amount.getAmount();
 
-	chosenJ = -1;
-	double minR = 0;
-	
-	for(int j=0; j< givenToEach.size(); j++) {
-	    if (fractions.get(j)==0) continue;
-	    double r = (givenToEach.get(j)+amt)/fractions.get(j);	    
-	    if (chosenJ < 0 || r < minR) {
-		chosenJ = j;
-		minR = r;
+	Receiver chosenR = null;
+	double minZ = 0;
+
+	for(Receiver r: receivers) {
+	    RData d = data.get(r);
+	    if (d.fraction==0) continue;
+	    double z = (d.given+amt)/d.fraction;	    
+	    if (chosenR==null || z < minZ) {
+		chosenR = r;
+		minZ = z;
 	    }
 	}
-	//System.out.println("Chose j="+ chosenJ +", for amt=" + lastAmt);
-	Receiver recv = receivers.get(chosenJ);
-	return recv;
+	System.out.println("Chose: " +chosenR);
+	if (chosenR==null)  throw new IllegalArgumentException("Something wrong: chose null out of "+ receivers.size()+" receivers!");
+	return chosenR;
     }
         
     public String toString()
@@ -142,21 +122,15 @@ public class Splitter extends If {
         }
 
     /** Does nothing.  There's no reason to step a Splitter. */
-    public void step(SimState state)
-        {
+    //public void step(SimState state)        {
         // do nothing
-        }
+//}
         
-    //    boolean refusesOffers = false;
-    //	public void setRefusesOffers(boolean value) { refusesOffers = value; }
-    //    public boolean getRefusesOffers() { return refusesOffers; }
-
-
     public String report() {
-	String s = "Split "+	totalAcecepted+" ba: ";
+	String s = "Split "+	totalAccepted+" units: ";
 	Vector<String> v=new Vector<>();
-	for(int j=0; j< givenToEach.size(); j++) {
-	    v.add("To " + myReceivers.get(j).getName() + ", " + givenToEach.get(j));
+	for(Receiver r: data.keySet()) {
+	    v.add("To " + r.getName() + ", " + data.get(r).given);
 	}
 	s += String.join("; ", v);
 	
